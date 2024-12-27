f9c234a453f4bf3b6bbb0ac653d4ade6c1495b5a
diachkinainna <diachkinainna@gmail.com>
2024-12-27 17:14:31 +0200

    fixed coordinates

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,8 @@ const wall = document.querySelector('.wall');
 
 document.addEventListener('click', (e) => {
   const wallPosition = wall.getBoundingClientRect();
-  let topPosition = e.clientY - wallPosition.top - spider.clientWidth / 2;
-  let leftPosition = e.clientX - wallPosition.left - spider.clientHeight / 2;
+  let topPosition = e.clientY - wallPosition.top - spider.clientHeight / 2;
+  let leftPosition = e.clientX - wallPosition.left - spider.clientWidth / 2;
 
   leftPosition = Math.max(
     0,
